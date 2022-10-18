525d52ace1c7e00b06c7467724ea46d84a4c879e
“Natali <natali-kontakt@yandex.ru>
2022-10-18 20:38:30 +0500

    100 percent

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,65 @@
+package ru.netology.radio;
+
+public class Radio {
+
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentRadioStation(int CurrentRadioStation) {
+        if (CurrentRadioStation < 0) {
+            return;
+        }
+        if (CurrentRadioStation > 9) {
+            return;
+        }
+        this.currentRadioStation = CurrentRadioStation;
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        this.currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+}
